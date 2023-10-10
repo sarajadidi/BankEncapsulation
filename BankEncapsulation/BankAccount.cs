47da23ade8424c80c564a1ca0fc100059978874f
sarajadidi <sararivkajadidi@gmail.com>
2023-10-09 18:10:17 -0700

    Finished the exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		public BankAccount()
+		{
+		}
+
+
+		private double balance = 0;
+
+		public void Deposit(double amount)
+		{
+			balance = amount;
+		}
+
+		public string GetBalance()
+		{
+			return $"${balance}";
+		}
+		
+	}
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("  ___            _       _                      _   ");
+            Console.WriteLine(" | _ ) __ _ _ _ | |__   /_\\  __ __ ___ _  _ _ _| |_ ");
+            Console.WriteLine(" | _ \\/ _` | ' \\| / /  / _ \\/ _/ _/ _ \\ || | ' \\  _|");
+            Console.WriteLine(" |___/\\__,_|_||_|_\\_\\ /_/ \\_\\__\\__\\___/\\_,_|_||_\\__|");
+
+
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            var sarasAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            sarasAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {sarasAccount.GetBalance()}");
+
+            
         }
     }
 }
